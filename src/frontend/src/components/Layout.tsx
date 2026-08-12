438797cc65a00426edc198a881138b9de019596a
Gold DAO <info@goldao.app>
2026-08-12 11:49:46 -0300

    Update Layout.tsx

--- src/frontend/src/components/Layout.tsx
+++ src/frontend/src/components/Layout.tsx
@@ -1,19 +1,23 @@
 import { Outlet } from "@tanstack/react-router";
 
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
+import { MobileTabBar, Sidebar } from "@/components/Navbar";
 
 export default function Layout() {
   return (
     <div
       data-ocid="layout"
-      className="flex min-h-screen flex-col bg-background"
+      className="flex min-h-screen bg-background"
     >
-      <Navbar />
-      <main data-ocid="main" className="flex-1 bg-background animate-fade-in">
+      {/* Desktop sidebar */}
+      <Sidebar />
+
+      {/* Main content */}
+      <main data-ocid="main" className="flex-1 min-w-0 bg-background animate-fade-in pb-16 md:pb-0">
         <Outlet />
       </main>
-      <Footer />
+
+      {/* Mobile bottom tab bar */}
+      <MobileTabBar />
     </div>
   );
 }
